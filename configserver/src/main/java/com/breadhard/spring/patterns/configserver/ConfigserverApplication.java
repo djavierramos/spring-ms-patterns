0d570de6141b8503c3849ae5b4edcb8c872e3e47
Javier Ramos Panduro <javier.ramos@inetum.com>
2024-02-29 12:16:54 +0100

    Incluir config server

---
+++ configserver/src/main/java/com/breadhard/spring/patterns/configserver/ConfigserverApplication.java
@@ -0,0 +1,15 @@
+package com.breadhard.spring.patterns.configserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class ConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserverApplication.class, args);
+    }
+
+}
--- customers-service/src/main/java/com/breadhard/spring/patterns/customersservice/infraestructure/rest/controller/CustomersController.java
+++ customers-service/src/main/java/com/breadhard/spring/patterns/customersservice/infraestructure/rest/controller/CustomersController.java
@@ -2,6 +2,9 @@ package com.breadhard.spring.patterns.customersservice.infraestructure.rest.cont
 
 import java.util.UUID;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.breadhard.spring.patterns.customersservice.infraestructure.rest.dto.GetCustomerResponseDTO;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/customers")
 @CrossOrigin
 @RequiredArgsConstructor
+@Slf4j
 public class CustomersController {
     @GetMapping("/{customer-id}")
     Mono<GetCustomerResponseDTO> getCustomer(@PathVariable("customer-id") String customerId) {
+        log.info(String.format("Get the customer %s",customerId));
         return Mono.just(GetCustomerResponseDTO.builder().id(UUID.randomUUID().toString()).name("John Doe").build());
     }
 }
